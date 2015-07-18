650ab51c9f8b31c333ff9cce19b14f4db6242f0c
Edward Bridges <ebridges@squarespace.com>
2015-07-17 23:28:29 -0400

    basic login status

---
+++ js/components/LoginStatus.react.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let LoginStatus = React.createClass({
+  render: function() {
+    return (
+      <div>login status</div>
+    );
+  }
+});
+
+export default LoginStatus;
