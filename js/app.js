0f1c3cdfe613bc7f171bd3d89f3a2fd8a02cbad0
Edward Q. Bridges <ebridges@roja.cc>
2015-07-18 07:39:24 -0400

    paramterize data for Ledger from app level.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Ledger from './components/Ledger.react.js';
 
-React.render(<Ledger/>, document.body);
+var username = "ebridges";
+var categories = ["Categories...", "Meals", "Groceries", "Entertainment"];
+var columnLabels = ["Txn Date", "Category", "Description", "Description (orig)", "Amount"];
+var transactions = [
+  {"txnDate": "2010-01-01", "categoryName": "Fruit", "description": "Apple purchase", "originalDescription" : "Apple purchase 0x1212", "amount": "$10.00"},
+  {"txnDate": "2010-01-02", "categoryName": "Fruit", "description": "Orange purchase", "originalDescription" : "Orange purchase 0x1212", "amount": "$20.00"},
+];
+
+React.render(<Ledger username={username} categories={categories} columnLabels={columnLabels} transactions={transactions} />, document.body);
 
 
 // import React from 'react';
--- js/components/Ledger.react.js
+++ js/components/Ledger.react.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
-var username = "ebridges";
-var categories = ["Categories...", "Meals", "Groceries", "Entertainment"];
-var columnLabels = ["Txn Date", "Category", "Description", "Description (orig)", "Amount"];
-var transactions = [
-  {"txnDate": "2010-01-01", "categoryName": "Fruit", "description": "Apple purchase", "originalDescription" : "Apple purchase 0x1212", "amount": "$10.00"},
-  {"txnDate": "2010-01-02", "categoryName": "Fruit", "description": "Orange purchase", "originalDescription" : "Orange purchase 0x1212", "amount": "$20.00"},
-];
-
 let Ledger = React.createClass({
   render: function() {
     return (
       <div class="ledger">
         <div><h1>Ledger</h1></div>
-        <div><LoginStatus username={username} /></div>
+        <div><LoginStatus username={this.props.username} /></div>
         <div><DateRangeCriteria /></div>
-        <div><CategoryList categories={categories} /></div>
-        <div><TransactionTable columnLabels={columnLabels} transactions={transactions} /></div>
+        <div><CategoryList categories={this.props.categories} /></div>
+        <div><TransactionTable columnLabels={this.props.columnLabels} transactions={this.props.transactions} /></div>
       </div>
     );
   }
 });
 
-//React.render(<Ledger username={username} categories={categories} columnLabels={columnLabels} transactions={transactions} />, document.body);
 export default Ledger;
