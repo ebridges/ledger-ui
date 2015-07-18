b24607e030c50d67af95a995022c7140e6bbc10e
Edward Q. Bridges <ebridges@roja.cc>
2015-07-18 07:00:35 -0400

    render the Ledger component

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import Router from 'react-router';
-import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+import Ledger from './components/Ledger.react.js';
 
-import LoginHandler from './components/Login.js';
+React.render(<Ledger/>, document.body);
 
-let App = React.createClass({
-  render() {
-    return (
-      <div className="nav">
-        <Link to="app">Home</Link>
-        &nbsp;&#x2022;&nbsp;
-        <Link to="login">Login</Link>
-        <RouteHandler/>
-      </div>
-    );
-  }
-});
 
-let routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="login" path="/login" handler={LoginHandler}/>
-  </Route>
-);
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.body);
-});
+// import React from 'react';
+// import Router from 'react-router';
+// import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+//
+// import Ledger from './components/Ledger.react.js';
+//
+// let App = React.createClass({
+//   render() {
+//     return (
+//       <div className="ledger">
+//         <RouteHandler/>
+//         <Ledger/>
+//       </div>
+//     );
+//   }
+// });
+//
+// let routes = (
+//   <Route name="app" path="/" handler={App}>
+//     <Route name="login" path="/login" handler={LoginHandler}/>
+//   </Route>
+// );
+//
+// Router.run(routes, function (Handler) {
+//   React.render(<Handler/>, document.body);
+// });
