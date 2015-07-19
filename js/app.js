1515baf08b94979b3d84e97de72ad44ff8bb2a87
Edward Bridges <ebridges@squarespace.com>
2015-07-19 14:27:54 -0400

    add `id` property to map to the keys of iterated collns

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Ledger from './components/Ledger.react.js';
 
 var username = "ebridges";
-var categories = ["Categories...", "Meals", "Groceries", "Entertainment"];
+var categories = [
+  {"id": 1, "name": "Categories..."},
+  {"id": 2, "name": "Meals"},
+  {"id": 3, "name": "Groceries"},
+  {"id": 4, "name": "Entertainment"}
+];
 var columnLabels = ["Txn Date", "Category", "Description", "Description (orig)", "Amount"];
 var transactions = [
-  {"txnDate": "2010-01-01", "categoryName": "Fruit", "description": "Apple purchase", "originalDescription" : "Apple purchase 0x1212", "amount": "$10.00"},
-  {"txnDate": "2010-01-02", "categoryName": "Fruit", "description": "Orange purchase", "originalDescription" : "Orange purchase 0x1212", "amount": "$20.00"},
+  {"id": 1, "txnDate": "2010-01-01", "categoryName": "Fruit", "description": "Apple purchase", "originalDescription" : "Apple purchase 0x1212", "amount": "$10.00"},
+  {"id": 2, "txnDate": "2010-01-02", "categoryName": "Fruit", "description": "Orange purchase", "originalDescription" : "Orange purchase 0x1212", "amount": "$20.00"},
 ];
 
 React.render(<Ledger username={username} categories={categories} columnLabels={columnLabels} transactions={transactions} />, document.body);
@@ -17,13 +22,27 @@ React.render(<Ledger username={username} categories={categories} columnLabels={c
 // import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 //
 // import Ledger from './components/Ledger.react.js';
+// import LoginHandler from './components/LoginHandler.react.js';
+//
+// var username; // = "ebridges";
+// var categories = [
+//   {"id": 1, "name": "Categories..."},
+//   {"id": 2, "name": "Meals"},
+//   {"id": 3, "name": "Groceries"},
+//   {"id": 4, "name": "Entertainment"}
+// ];
+// var columnLabels = ["Txn Date", "Category", "Description", "Description (orig)", "Amount"];
+// var transactions = [
+//   {"id": 1, "txnDate": "2010-01-01", "categoryName": "Fruit", "description": "Apple purchase", "originalDescription" : "Apple purchase 0x1212", "amount": "$10.00"},
+//   {"id": 2, "txnDate": "2010-01-02", "categoryName": "Fruit", "description": "Orange purchase", "originalDescription" : "Orange purchase 0x1212", "amount": "$20.00"},
+// ];
 //
 // let App = React.createClass({
 //   render() {
 //     return (
 //       <div className="ledger">
 //         <RouteHandler/>
-//         <Ledger/>
+//         <Ledger username={username} categories={categories} columnLabels={columnLabels} transactions={transactions} />
 //       </div>
 //     );
 //   }
