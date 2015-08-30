4742c7059c8cc831642dd207b0e36200d8c43f5a
Edward Q. Bridges <ebridges@roja.cc>
2015-08-30 16:32:12 -0400

    react is case sensitive

--- js/components/TransactionTableHeader.react.js
+++ js/components/TransactionTableHeader.react.js
@@ -4,7 +4,7 @@ let TransactionTableHeader = React.createClass({
   render: function() {
     return (
       <tr>
-        <th colspan="2">{this.props.columnLabels[0]}</th>
+        <th colSpan="2">{this.props.columnLabels[0]}</th>
         <th>{this.props.columnLabels[1]}</th>
         <th>{this.props.columnLabels[2]}</th>
         <th>{this.props.columnLabels[3]}</th>
