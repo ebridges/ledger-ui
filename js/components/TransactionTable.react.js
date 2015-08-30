35635301abbfdb44014f256e507296952a1b8a5d
Edward Q. Bridges <ebridges@roja.cc>
2015-08-30 16:32:45 -0400

    add a css classname

--- js/components/TransactionTable.react.js
+++ js/components/TransactionTable.react.js
@@ -5,10 +5,11 @@ import TransactionTableHeader from './TransactionTableHeader.react.js';
 let TransactionTable = React.createClass({
     render: function() {
       let rows = [];
-      this.props.transactions.forEach(function(transaction) {
+      var transactionCount = this.props.transactions.length;
+      for(var i=0; i<transactionCount; i++) {
+        var transaction = this.props.transactions[i];
         rows.push(<TransactionRow transaction={transaction} key={transaction.id} />);
-      });
-
+      }
       return (
         <table>
           <thead>
