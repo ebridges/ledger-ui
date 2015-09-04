27143f386d79f4649459a18ec26f59ac1b0c8dcb
Edward Q. Bridges <ebridges@roja.cc>
2015-09-04 08:34:29 -0400

    basic table styling & styling of checkbox

--- js/components/TransactionRow.jsx
+++ js/components/TransactionRow.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 let TransactionRow = React.createClass({
   render: function() {
+    let t = this.props.transaction;
+    let tid = "t" + t.id;
     return (
       <tr>
-        <td><input type="checkbox"/></td>
-        <td>{this.props.transaction.txnDate}</td>
-        <td>{this.props.transaction.categoryName}</td>
-        <td>{this.props.transaction.description}</td>
-        <td>{this.props.transaction.originalDescription}</td>
-        <td>{this.props.transaction.amount}</td>
+        <td className="checkbox-region">
+          <input id={tid} type="checkbox" name="transactionId" value={t.id}/>
+          <label htmlFor={tid}/>
+        </td>
+        <td>{t.txnDate}</td>
+        <td>{t.categoryName}</td>
+        <td>{t.description}</td>
+        <td>{t.originalDescription}</td>
+        <td>{t.amount}</td>
       </tr>
     );
   }
