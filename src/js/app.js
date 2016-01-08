ac9b089b1ab0cfb1ad8188b91d476863486e063e
Edward Q. Bridges <ebridges@roja.cc>
2016-01-07 21:54:17 -0500

    label/title tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Ledger from './components/Ledger.jsx';
+import LedgrStore from './store/LedgrStore';
+import { Provider } from 'react-redux';
+import { configure } from "redux-auth";
 
-React.render(<Ledger/>, document.body);
+require('../css/reset.css');
+require('../css/common.css');
+
+var store = LedgrStore.store();
+
+// maybe move this to LedgrStore?
+store.dispatch(configure({
+  apiUrl: "http://localhost:9000"
+}));
+
+ReactDOM.render(
+  <Provider store={ store }>
+      <Ledger/>
+  </Provider>,
+  document.getElementById('ledgr')
+);
+
