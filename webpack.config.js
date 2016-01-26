1d2f04835e75d060f5b002984416ece4682f309c
Edward Bridges <github@eqbridges.com>
2016-01-25 21:57:57 -0500

    add configuration tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 var webpack = require('webpack');
 module.exports = {
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
     entry: [
       "./src/js/app.js",
       'webpack/hot/only-dev-server'
@@ -14,7 +19,9 @@ module.exports = {
         contentBase: './dist'
     },
     module: {
+        noParse: /node_modules\/json-schema\/lib\/validate\.js/,
         loaders: [
+            { test: /\.json$/, loader: "json-loader" },
             { test: /\.js.?$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/ },
             { test: /\.css$/, loaders: ['style', 'css'], exclude: /node_modules/ }
         ]
