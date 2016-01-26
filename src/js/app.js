ff32effcefe1a1d6860e0a4fe7a494339ec0dec9
Edward Bridges <github@eqbridges.com>
2016-01-25 22:24:03 -0500

    add middleware for logging actions

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,15 @@ import Ledger from './components/Ledger.jsx';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import ledgerReducer from './reducers/LedgerReducer';
 
 require('../css/reset.css');
 require('../css/common.css');
 
-let createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+let logger = createLogger();
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, promise, logger)(createStore);
 
 let store = createStoreWithMiddleware(ledgerReducer);
 
